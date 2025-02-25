b51d755ab6233d3500b634480e578e3a249ce958
Adam Ault <91794118+awault@users.noreply.github.com>
2025-02-25 19:40:08 +0000

    Add package data for mystyles.mplstyle

--- setup.py
+++ setup.py
@@ -5,9 +5,12 @@ setup(
     version="0.1.0",
     packages=find_packages(),
     install_requires=[
-        "matplotlib",
+        "matplotlib>=3.4",
         "seaborn"
     ],
+    package_data={
+        "custom_charts": ["styles/mystyles.mplstyle"]
+    },
     classifiers=[
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent"
