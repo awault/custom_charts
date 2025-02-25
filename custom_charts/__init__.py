bd7739546da28932f3655d211b394c7a5561d97a
Adam Ault <91794118+awault@users.noreply.github.com>
2025-02-25 02:42:09 +0000

    Add basic settings in custom_chart

--- custom_charts/__init__.py
+++ custom_charts/__init__.py
@@ -4,5 +4,10 @@ import matplotlib.pyplot as plt
 import matplotlib as mpl
 
 def custom_charts(data):
+    plt.figure(figsize=(8,4))
+    # Create Watermark
+    plt.figtext(x=0.89, y=0.12, s="©2025 New Insight Analytics", 
+                color='#054f70', fontsize=10, weight='ultralight',
+                ha='right', va='bottom', transform=plt.gcf().transFigure)
     plt.plot(data)
     plt.show()
